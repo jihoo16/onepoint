cd6fb9cc05729287acb3e96d34ba1453f97a9dfa
jihoo16 <154419004+jihoo16@users.noreply.github.com>
2023-12-27 20:20:49 +0900

    금일 작업 내용 업로드
    we 페이지 완성

--- js/weScript01.js
+++ js/weScript01.js
@@ -7,7 +7,7 @@ $(function(){
 	
 	closeOpen.hide();
 	open.click(function(){
-		closeOpen.show();
+		closeOpen.slideDown(1000);
 		$("body").addClass('hidden');
 		close.show();
 		open.hide();
@@ -18,5 +18,21 @@ $(function(){
 		close.hide();
 		$("body").removeClass('hidden');
 	})
+	AOS.init();
+	var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
+	// alert(scrollBottom);
+	$(window).scroll(function(){
+        // 스크롤이 페이지 하단에 도달하면
+        if ($(this).scrollTop() > 3513) {
+          $('.upup').fadeIn();
+        } else {
+          $('.upup').fadeOut();
+        }
+      });
 
+      // 맨 위로 스크롤 이동
+      $('.upup').click(function(){
+        $('html, body').animate({scrollTop : 0}, 0);
+        return false;
+      });
 });
