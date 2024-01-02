60cff5d841c203b5e26fe2f875827c0a01219ff7
jihoo16 <154419004+jihoo16@users.noreply.github.com>
2024-01-02 20:00:05 +0900

    weApply.html 제작
    index- nav 수정
    we - nav img 수정

--- js/weApplyScript01.js
+++ js/weApplyScript01.js
@@ -35,19 +35,78 @@ $(function(){
 		$('html, body').animate({scrollTop : 0}, 0);
 		return false;
 	});
-	let all = $(".all");
+	let allj = $(".allj");
+	let alljob = $(".alljob");
+	let allp = $(".allp");
+	let allposition = $(".allposition");
 	let backend = $(".Backend");
 	let marketer = $(".Marketer");
 	let growth = $(".growth");
 	let full = $(".full");
+	let all = $(".all");
 	let back = $(".back");
+	let business = $(".business");
+	let product =$(".product");
+	let allapply = $(".allapply");
+	let alla = $(".alla");
+	let fulltime = $(".fulltime");
+	let contrat = $(".contrat");
+	let intern = $(".intern");
+
 	backend.click(function(){
 		growth.hide();
 		full.show();
+		alljob.text('Backend Developer');
 	})
 	marketer.click(function(){
 		full.hide();
 		growth.show();
+		alljob.text('Marketer');
+	})
+	allj.click(function(){
+		alljob.text('전체 직군');
+		full.show();
+		growth.show();
+		
+	})
+
+	intern.click(function(){
+		growth.hide();
+		full.show();
+		allapply.text('인턴');
+	})
+	fulltime.click(function(){
+		full.hide();
+		growth.show();
+		allapply.text('정규직');
+	})
+	contrat.click(function(){
+		allapply.text('계약직');
+		full.show();
+		growth.show();
+	})
+	alla.click(function(){
+		allapply.text('전체 고용 형태');
+		full.show();
+		growth.show();
+		
+	})
+
+	product.click(function(){
+		growth.hide();
+		full.show();
+		allposition.text('프로덕트');
+	})
+	business.click(function(){
+		full.hide();
+		growth.show();
+		allposition.text('Business');
+	})
+	allp.click(function(){
+		allposition.text('전체 소속');
+		full.show();
+		growth.show();
+		
 	})
 	all.click(function(){
 		full.show();
